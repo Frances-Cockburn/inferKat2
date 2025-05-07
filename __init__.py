fbd294ae1623655c45b3079127b0d3146d868350
Frances-Cockburn <fcockbu1@jhu.edu>
2025-05-07 13:42:19 -0400

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,5 +1,1 @@
 
-!pip install scanpy scipy umap-learn leidenalg
-!pip install infercnvpy
-!pip install hmmlearn
-!pip install pykalman
--- setup.py
+++ setup.py
@@ -5,7 +5,15 @@ setup(
     name='inferKat2',
     version='0.1',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        'scanpy',
+        'scipy',
+        'umap-learn',
+        'leidenalg',
+        'infercnvpy',
+        'hmmlearn', 
+        'pykalman',
+    ],
     author='CEKN',
     description='CSCB CEKN Final Project Package',
 )
